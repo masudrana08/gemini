908034acb79968224d078db880b298b8fd62d093
masudx08 <promasud225@gmail.com>
2022-02-28 11:56:31 +0600

    responsive almost done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,6 +7,7 @@ import {AiFillInstagram} from 'react-icons/ai'
 import AppStore from '../../images/appstore.svg'
 import PlayStore from '../../images/playstore.svg'
 import GalaxyStore from '../../images/galaxy.svg'
+import { Col, Row } from 'react-bootstrap'
 export default function Footer() {
   return (
     <div className='footerC'>
@@ -41,11 +42,7 @@ export default function Footer() {
           </div>
           <div>
             <FaFacebookF />
-          </div>
-          
-          
-          
-          
+          </div> 
         </div>
         <div className='right'>
           <img src={AppStore } alt="" />
@@ -53,12 +50,19 @@ export default function Footer() {
           <img src={GalaxyStore} alt="" />
         </div>
       </div>
-      <div className="footer-foot">
-        <p>© Copyright 2022 Gemini Trust Company, LLC.</p>
-        <p>NMLS #1518126</p>
-        <p>Legal Notice</p>
-        <p>This site is protected by reCAPTCHA and the Google Privacy Policy and Terms of Service apply.</p>
-      </div>
+      <Row className="footer-foot m-0">
+        <Col xs={12} md={4} className='a'>
+          <p>© Copyright 2022 Gemini Trust Company, LLC.</p>
+        </Col>
+
+        <Col xs={12} md={4} className='b'>
+          <p>NMLS #1518126</p>
+          <p>Legal Notice</p>
+        </Col>
+        <Col xs={12} md={4} className='c'>
+          <p>This site is protected by reCAPTCHA and the Google Privacy Policy and Terms of Service apply.</p>
+        </Col>
+      </Row>
     </div>
   )
 }
