259737764af4d1b65f16cf684fe1b3809830aa7f
masudx08 <promasud225@gmail.com>
2022-02-28 12:57:59 +0600

    done

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -12,7 +12,9 @@ export default function Faq() {
             <Accordion.Body>
               <p>Gemini Earn is a lending program through which you may choose to lend your crypto to certain institutional borrowers and earn interest on your crypto.</p>
               <p>On the Gemini platform, customers can view their combined trading balance and Earn balance, as well as the interest they’ve earned. Interest is paid daily and will start accruing at 4 p.m. ET on the next business day after funds are moved to Gemini Earn.</p>
-              <p>Learn more</p>
+              <p>
+                <a href="https://exchange.gemini.com/register?referral=67k9yz9se#">Learn More</a>
+              </p>
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="1">
@@ -20,7 +22,9 @@ export default function Faq() {
             <Accordion.Body>
               <p>Gemini is partnering with accredited third party borrowers including Genesis, who are vetted through our risk management framework which reviews our partners’ collateralization management process. On a periodic basis we will conduct an analysis of our partners’ cash flow, balance sheet, and financial statements to ensure the appropriate risk ratios and healthy financial condition of our partners.</p>
               <p>Cryptocurrency, like many assets, can be volatile and subject to price swings. There is always a risk in investing, and each customer needs to assess their own risk tolerance before making any investment decisions. Our partners in Gemini Earn have an obligation to return funds according to the terms of their loan agreement. However, Gemini Earn customers (the lenders) always assume some level of risk when they decide to lend their funds. We believe Gemini Earn gives our retail investors another way to stay long-term in the asset class and have the option to invest and earn interest, all on the Gemini platform.</p>
-              <p>Learn more</p>
+              <p>
+                <a href="https://exchange.gemini.com/register?referral=67k9yz9se#">Learn More</a>
+              </p>
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="2">
@@ -28,7 +32,9 @@ export default function Faq() {
             <Accordion.Body>
               <p>Typically, Gemini can process your redemption quickly after you request your funds. If our partners receive a high volume of redemption requests during the same period, it may take longer for them to respond to each request. However, in all cases our partners are required to return your funds to you within five business days.</p>
               <p>You will need to redeem your crypto before you use that balance to trade. Redeeming from Earn moves your crypto from your Earn balance to your trading balance. You can easily view both your trading and Earn balances on the “My Portfolio” tab on the App and on the Web. ActiveTrader users can view balances on the Account tab.</p>
-              <p>Learn more</p>
+              <p>
+                <a href="https://exchange.gemini.com/register?referral=67k9yz9se#">Learn More</a>
+              </p>
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="3">
@@ -53,7 +59,9 @@ export default function Faq() {
             <Accordion.Header>Why would interest rates vary over time?</Accordion.Header>
             <Accordion.Body>
               <p>Rates vary based on the supply and demand for each cryptocurrency in crypto lending markets, similar to how interest rates vary for any type of currency.</p>
-              <p>Learn more</p>
+              <p>
+                <a href="https://exchange.gemini.com/register?referral=67k9yz9se#">Learn More</a>
+              </p>
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="5">
@@ -62,7 +70,9 @@ export default function Faq() {
               <p>Your funds in Earn are not insured by Gemini but are held with our trusted partners. Our partners are vetted through our risk management framework and always disclosed to you, so you know which institution has borrowed your funds. Currently, Gemini is partnering with accredited third party borrower Genesis.</p>
               <p>Additionally, Gemini Earn is structured similarly to many non-deposit services offered by financial institutions and not insured by FDIC, SIPC, any other governmental program, or Gemini. All loans are open-term and callable, and the customer’s experience is seamless with Gemini’s platform, allowing quick access to earn interest and redeem funds.</p>
               <p>For GUSD in Earn, while the U.S. dollar reserves backing the GUSD tokens are eligible for FDIC insurance, the GUSD tokens themselves are not insured, whether or not in Earn.</p>
-              <p>Learn more</p>
+              <p>
+                <a href="https://exchange.gemini.com/register?referral=67k9yz9se#">Learn More</a>
+              </p>
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
